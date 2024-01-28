30d66ec789af4b1b3ac32f82d64265744a6bf3cf
ExedNetic <157895999+ExedNetic@users.noreply.github.com>
2024-01-28 19:02:10 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void GenTempPassword(char *TempPassword, unsigned int seed, int legth)
+{
+  char characters[] = "!#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ^_`abcdefghijklmnopqrstuvwxyz|~";
+  int charactersLegth = sizeof(characters) - 1;
+  srand(seed);
+  for (int c = 0; c < legth; c++)
+  {
+    int random = rand() % charactersLegth;
+    TempPassword[c] = characters[random];
+  }
+  TempPassword[legth] = '\0';
+}
+
+int main()
+{
+  int Length = 12;
+  char password[Length + 1];
+  unsigned int seed;
+
+  printf("Enter a random number: \n");
+  scanf("%d", &seed);
+
+  GenTempPassword(password, seed, Length);
+
+  printf("Your password is: %s\n", password);
+}
